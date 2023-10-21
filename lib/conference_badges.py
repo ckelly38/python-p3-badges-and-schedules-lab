ce18269ce76519708deb261ab9195202225d0505
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-10-20 21:32:38 -0600

    this takes some data and generates more using list comprehensions and for loops for a list of names

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,28 @@
 def badge_maker(name):
-    return None
+    if (name == None or len(name) < 1): raise Exception("you must enter a valid name!");
+    else: return f"Hello, my name is {name}.";
 
 def batch_badge_creator(names):
-    return None
+    if (names == None): return None;
+    elif (len(names) < 1): return [];
+    else: return [badge_maker(name) for name in names];
 
 def assign_rooms(names):
-    return None
+    if (names == None): return None;
+    elif (len(names) < 1): return [];
+    else:
+        rmasmnts = ["" for i in range(len(names))];
+        for i in range(len(names)):
+            rmasmnts[i] = f"Hello, {names[i]}! You'll be assigned to room {(i + 1)}!";
+        return rmasmnts;
 
 def printer(names):
-    return None
+    mybadges = batch_badge_creator(names);
+    for badge in mybadges:
+        print(badge);
+    rmnums = assign_rooms(names);
+    for rmnum in rmnums:
+        print(rmnum);
+    return None;
+
+printer(["Alan", "Tron", "Clu", "Kevin Flynn"]);
